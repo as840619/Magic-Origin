5332f7f46c3083d1b5fdbd7983aca44259719306
HunterM <as22281624@gmail.com>
2023-02-23 19:22:35 +0800

    Bug fix

---
+++ Assets/Sciprts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("基本數據")]
+    public int nearInfectorEnemyHP;
+}
