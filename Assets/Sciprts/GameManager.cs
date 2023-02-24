016461a2eb490e45a42909dc95ea8e657425b325
HunterM <as22281624@gmail.com>
2023-02-24 15:43:46 +0800

    Add basic data

--- Assets/Sciprts/GameManager.cs
+++ Assets/Sciprts/GameManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     [Header("基本數據")]
-    public int nearInfectorEnemyHP;
+    public static int enemyInfectorFarHP;
+    public static int enemyInfectorNearHP;
+    public static int enemyInfectorBossHP;
+    public static int enemyHP;
+    [Header("介面控制")]
+    [SerializeField] Text _enemyInfectorFarHPText;
+    [SerializeField] Text _enemyInfectorNearHPText;
+    [SerializeField] Text _enemyInfectorBossHPText;
+
 }
