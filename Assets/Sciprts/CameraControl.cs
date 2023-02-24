a126cc223a6821ed91b5cb6f22ffb9eb1c148e8e
HunterM <as22281624@gmail.com>
2023-02-24 15:43:26 +0800

    Add camera follow player
    Add offset and move smoothly

--- Assets/Sciprts/CameraControl.cs
+++ Assets/Sciprts/CameraControl.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] Transform _idle;
-    [SerializeField] Vector2 _offset;
-    void Look()
+    [Header("基本數據")]
+    [SerializeField] GameObject _idle;
+    [SerializeField] float _offset;
+    [SerializeField] float _offsetSmoothing;
+    [SerializeField] Vector3 _cameraPosition;
+
+    void Start()
     {
-        
+        _cameraPosition = new Vector3(4, 0.6882281f, -10f);
     }
+
     void Update()
     {
-        
+        Look();
+    }
+
+    void Look()
+    {
+        _cameraPosition = new Vector3(_idle.transform.position.x, _idle.transform.position.y - 2, transform.position.z);
+        if (_idle.transform.localScale.x > 0f)
+        {
+            _cameraPosition = new Vector3(_cameraPosition.x + _offset, _cameraPosition.y, _cameraPosition.z);
+        }
+        else
+        {
+            _cameraPosition = new Vector3(_cameraPosition.x - _offset, _cameraPosition.y, _cameraPosition.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, _cameraPosition, _offsetSmoothing * Time.deltaTime);
     }
 }
