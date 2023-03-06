114fcd25247d4754a2f62c990ba96c7f22aa3a32
HunterM <as22281624@gmail.com>
2023-03-07 01:22:44 +0800

    Create EnemyControl.cs

---
+++ Assets/Sciprts/EnemyControl.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    [Header("°ò¥»¼Æ­È")]
+    [SerializeField] int p;
+    
+}
