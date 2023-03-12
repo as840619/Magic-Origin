ab37ee537c782f8afe2fd0ad7b7e16a08516ad08
HunterM <as22281624@gmail.com>
2023-03-12 21:12:51 +0800

    Update Enemy_Zombie.cs
    Tidy up

--- Assets/Sciprts/Enemy_Zombie.cs
+++ Assets/Sciprts/Enemy_Zombie.cs
@@ -1,17 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Enemy_Zombie : Enemy
 {
-
-    // Start is called before the first frame update
     public void Start()
     {
         base.Start();
     }
 
-    // Update is called once per frame
     public void Update()
     {
         base.Update();
