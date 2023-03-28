f9165293c9158aedb7aed614c9a5b4beea9e2de8
HunterM <as22281624@gmail.com>
2023-03-28 18:39:42 +0800

    Fix script

---
+++ Assets/Sciprts/CardUse.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardUse : MonoBehaviour
+{
+
+          public void OnMouseDown()
+    {
+        Debug.Log("基本數據");
+    }
+
+    private void Update()
+    {
+        
+    }
+  
+}
