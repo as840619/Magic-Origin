24be470f989c06f1302b3f22297582d98bca12db
HunterM <as22281624@gmail.com>
2023-05-08 17:28:20 +0800

    Update Enemy_Zombie.cs

--- Assets/Sciprts/Enemy_Zombie.cs
+++ Assets/Sciprts/Enemy_Zombie.cs
@@ -1,6 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class Enemy_Zombie : Enemy
 {
-    public new void Start()
+
+    /*public new void Start()
     {
         base.Start();
     }
@@ -8,5 +13,6 @@ public class Enemy_Zombie : Enemy
     public new void Update()
     {
         base.Update();
-    }
+    }*/
+
 }
