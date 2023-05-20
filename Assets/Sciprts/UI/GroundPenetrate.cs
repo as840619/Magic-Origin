ce2997964b67ff61521cc31f4ed35af2c76dbafe
HunterM <as22281624@gmail.com>
2023-05-20 16:07:08 +0800

    Update GroundPenetrate.cs

--- Assets/Sciprts/UI/GroundPenetrate.cs
+++ Assets/Sciprts/UI/GroundPenetrate.cs
@@ -4,35 +4,29 @@ public class GroundPenetrate : MonoBehaviour
 {
     [SerializeField] bool fallthough = false;
     PlayerController playerControls;
-    private PlatformEffector2D effector;
+    PlatformEffector2D _platformEffector;
     private void Start()
     {
-        effector = GetComponent<PlatformEffector2D>();
-        playerControls = GetComponent<PlayerController>();
+        _platformEffector = GetComponent<PlatformEffector2D>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        string collTag = (collision.gameObject.tag.ToString());
-        if (collTag == "player")
-        {
+        if (collision.collider.CompareTag("Player"))
             playerControls = collision.gameObject.GetComponent<PlayerController>();
-        }
     }
     public void OnCollisionStay2D(Collision2D collision)
     {
-        Debug.Log(1);
-        //GetType().GetProperty(name).GetValue();
         if (playerControls == null)
             return;
         if (playerControls._fallThough)
         {
-            effector.rotationalOffset = 180f;
+            _platformEffector.rotationalOffset = 180f;
             playerControls = null;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         playerControls = null;
-        effector.rotationalOffset = 0f;
+        _platformEffector.rotationalOffset = 0f;
     }
 }
