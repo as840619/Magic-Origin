a66561f814c904c1d11dfeb5492e349d402f5dbc
HunterM <77477559+as840619@users.noreply.github.com>
2023-10-04 16:00:51 +0800

    Ak zombie fix (#29)
    * Update Attack.anim
    
    Fix Fire pictures
    
    * Update Enemy.controller
    
    * Update MainGamingScene.unity
    
    * Tidy up
    
    * Update EnemyChese.cs
    
    * Add new attack mode
    
    -TripperTake is 3 to 1 fire mode

--- Assets/Sciprts/GameObject/EnemyShooting.cs
+++ Assets/Sciprts/GameObject/EnemyShooting.cs
@@ -6,38 +6,59 @@ public class EnemyShooting : MonoBehaviour
 {
     public GameObject bullet;
     public Transform bulletPos;
-
+    private Animator anim;
     private float timer;
     private GameObject Player;
-    // Start is called before the first frame update
+
     void Start()
     {
+        anim = GetComponent<Animator>();
         Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-
         float distance = Vector2.Distance(transform.position, Player.transform.position);
         Debug.Log(distance);
 
-        if(distance < 10)//®gÀ»½d³ò Shooting Range setup
+        if (distance < 5)   //Zombie Shooting Range setup
         {
             timer += Time.deltaTime;
             if (timer > 2)
             {
                 timer = 0;
-                shoot();
+                anim.SetTrigger("Fire");
+                Shoot();
             }
         }
-
+        if (distance >= 7.5f)   //Zombie Shooting Range setup
+        {
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
+                timer = 0;
+                anim.SetTrigger("Fire");
+                TripperTake();
+            }
+        }
     }
-    void shoot()
+    void Shoot()        //1 to 1
     {
-        Instantiate(bullet,bulletPos.position,Quaternion.identity);
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
     }
 
+    void TripperTake()      //3 to 1
+    {
+        for (int n = -1; n <= 1; n++)
+        {
+            Vector3 newPostion = Calculation(n);
+            Instantiate(bullet, bulletPos.position + newPostion, Quaternion.identity);
 
+        }
+    }
+    Vector3 Calculation(int i)
+    {
+        Vector3 newPostion = new Vector3(0f, -0.675f * i, 0f);
+        return newPostion;
+    }
 }
