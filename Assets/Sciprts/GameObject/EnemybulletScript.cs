5d7e390365c7b782e6f9378862164f16e1df1b71
HunterM <as22281624@gmail.com>
2023-10-12 07:49:22 +0800

    Update EnemybulletScript.cs
    -修正還有人不給我用UTF8編碼寫中文

--- Assets/Sciprts/GameObject/EnemybulletScript.cs
+++ Assets/Sciprts/GameObject/EnemybulletScript.cs
@@ -5,26 +5,24 @@ using UnityEngine;
 
 public class EnemybulletScript : MonoBehaviour
 {
+    public bool lerping = false;
     public float force;
+    public float reCallTime;
     public Vector2 way;
-    public GameObject Player;
 
     private float timer;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private GameObject Player;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Player = GameObject.FindGameObjectWithTag("Player");
-
         Vector3 direction = Player.transform.position - transform.position;
         rb.velocity = way.normalized * force;
-
-        float rot = Mathf.Atan2(-direction.y, -direction.x)*Mathf.Rad2Deg;//�l�u�ਤ��
-        transform.rotation=Quaternion.Euler(0,0,rot+90);//����
+        float rot = Mathf.Atan2(-way.y, -way.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
@@ -32,14 +30,14 @@ public class EnemybulletScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<PlayerHealth>().health -= 20; //�l�u���ˮ`bulletdamage
-            Destroy (gameObject);
+            other.gameObject.GetComponent<PlayerHealth>().health -= 20; //bulletdamage
+            Destroy(gameObject);
         }
     }
 }
