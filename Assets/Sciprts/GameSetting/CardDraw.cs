5ad182bf970b1d9c5de74fc71a511a8e32892ed6
HunterM <as22281624@gmail.com>
2023-10-20 07:52:53 +0800

    優化

--- Assets/Sciprts/GameSetting/CardDraw.cs
+++ Assets/Sciprts/GameSetting/CardDraw.cs
@@ -7,13 +7,12 @@ public class CardDraw : MonoBehaviour
     //int rank = 0;
     public GameObject CardS1;
     public GameObject CardS2;
-    [SerializeField] PlayerAttack _pa;
+
     public void OnClick()
     {
         for (int i = 0; i < 5; i++)
         {
             GameObject card = Instantiate(CardS1, new Vector2(5 + i * 55, 10), Quaternion.identity);
-            card.GetComponent<CardUse>().pa = _pa;
             card.transform.SetParent(GameObject.FindGameObjectWithTag("UUI").transform, false);
         }
     }
