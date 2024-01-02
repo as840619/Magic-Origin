138582ceebb068e70e9b0fcc9ccdbd7a62974bc6
HunterM <as22281624@gmail.com>
2024-01-02 16:18:27 +0800

    血條腳本小改位置
    -省去贅字

--- Assets/Sciprts/HealthBar.cs
+++ Assets/Sciprts/HealthBar.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
-    public  int HealthCurrent;//當前血量
-    public  int HealthMax;//最大血量
-    SpriteRenderer spr;
-    // Start is called before the first frame update
+    public int HealthCurrent;  //當前血量
+    public int HealthMax;  //最大血量
+    private SpriteRenderer spr;
+
     void Start()
     {
-        spr= GetComponent<SpriteRenderer>();
+        spr = GetComponent<SpriteRenderer>();
         HealthMax = transform.GetComponentInParent<PlayerHealth>().health;
     }
 
-    // Update is called once per frame
     void Update()
     {
         HealthCurrent = transform.GetComponentInParent<PlayerHealth>().health;
         float p = Mathf.Max(0, (float)HealthCurrent / HealthMax);
-        spr.size = new Vector2(p,1);
+        spr.size = new Vector2(p, 1);
     }
 }
