b340e0de0742497d33c464d5f1a01e0ef4d75ff2
HunterM <as22281624@gmail.com>
2024-01-11 01:09:13 +0800

    Update EnemyShooting.cs

--- Assets/Sciprts/GameObject/EnemyShooting.cs
+++ Assets/Sciprts/GameObject/EnemyShooting.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform bulletPos;
-    [SerializeField] private float gapFire = 0.5f;
-    [SerializeField] private GameObject player;
-    private  PlayerHealth playerHealth;
-    private float timer;
-    private int op;
-    private Animator anim;
-    private float opTimer;
-    private float temp = 2f;
+    public GameObject bullet;
+    public Transform bulletPos;
+    public float gapFire = 0.5f;
+    public GameObject player;
+    public PlayerHealth playerHealth;
+    public float timer;
+    public int op;
+    public Animator anim;
+    public float opTimer;
+    public float temp = 2f;
 
     void Start()
     {
@@ -22,8 +22,8 @@ public class EnemyShooting : MonoBehaviour
 
     void Update()
     {
-        float distance = Vector2.Distance(transform.position, playerPosition);
-        /*if (distance < 5)   //Zombie Shooting Range setup
+        /*float distance = Vector2.Distance(transform.position, playerPosition);
+        if (distance < 5)   //Zombie Shooting Range setup
         {
             timer += Time.deltaTime;
             if (timer > 2)
@@ -32,7 +32,7 @@ public class EnemyShooting : MonoBehaviour
                 anim.SetTrigger("Fire");
                 Shoot();
             }
-        }*/
+        }
         if (distance >= 12f)   //Zombie Shooting Range setup
             return;
         timer += Time.deltaTime;
@@ -75,12 +75,13 @@ public class EnemyShooting : MonoBehaviour
         {
             opTimer = 0;
             op = (op + 1) % 6;
-        }
+        }*/
     }
-    Vector3 playerPosition => PlayerController.Instance.Position;
-    Vector2 playerDirection => playerPosition - transform.position;
-    float playerAngle => Mathf.Atan2(playerDirection.y, playerDirection.x) * Mathf.Rad2Deg;
-    GameObject AngleFix(float angle)
+
+    public Vector3 playerPosition => PlayerController.Instance.Position;
+    public Vector2 playerDirection => playerPosition - transform.position;
+    public float playerAngle => Mathf.Atan2(playerDirection.y, playerDirection.x) * Mathf.Rad2Deg;
+    public GameObject AngleFix(float angle)
     {
         GameObject bulletFive = Instantiate(bullet, bulletPos.position, Quaternion.identity);
         Vector2 angleWay = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
@@ -88,12 +89,12 @@ public class EnemyShooting : MonoBehaviour
         return bulletFive;
     }
 
-    void Shoot()        //1 to 1
+    public void Shoot()        //1 to 1
     {
         AngleFix(playerAngle);
     }
 
-    void ShotgunShot()  //1 to 3
+    public void ShotgunShot()  //1 to 3
     {
         for (int i = -1; i <= 1; i++)
         {
@@ -102,7 +103,7 @@ public class EnemyShooting : MonoBehaviour
         }
     }
 
-    void Pentagram()    //0 to 5
+    public void Pentagram()    //0 to 5
     {
         float angleAim = playerAngle;
         for (int m = 0; m <= 5; m++)
@@ -112,12 +113,12 @@ public class EnemyShooting : MonoBehaviour
         }
     }
 
-    void WrongFuckingShoot()    //0 to 5
+    public void WrongFuckingShoot()    //0 to 5
     {
         GameObject bulletShit = AngleFix(Mathf.Atan2(playerDirection.x, playerDirection.y) * Mathf.Rad2Deg);
     }
 
-    void LerpShot()     //lerp 1 to 1
+    public void LerpShot()     //lerp 1 to 1
     {
         GameObject bulletLerp = Instantiate(bullet, bulletPos.position, Quaternion.identity);
         bulletLerp.GetComponent<EnemybulletScript>().way = playerDirection;
@@ -126,12 +127,12 @@ public class EnemyShooting : MonoBehaviour
 
     //捲捲面
 
-    Vector2 Calculation(int i)
+    public Vector2 Calculation(int i)
     {
         Vector2 newPosition = new(0f, -0.675f * i);
         return newPosition;
     }
-    void TripperTake()      //3 to 1
+    public void TripperTake()      //3 to 1
     {
         for (int n = -1; n <= 1; n++)
         {
@@ -142,7 +143,7 @@ public class EnemyShooting : MonoBehaviour
         }
     }
 
-    void Raining()      //  _|_
+    public void Raining()      //  _|_
     {
         float middlePoint = (playerPosition.x + bulletPos.transform.position.x) / 2;
         GameObject bulletRain = Instantiate(bullet, new(middlePoint, (2 * bulletPos.position.y)), Quaternion.identity);
