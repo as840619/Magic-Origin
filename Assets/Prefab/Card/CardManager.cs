1c6b5bc466d3403bb85c8c0108a28b4fa1b7feda
HunterM <as22281624@gmail.com>
2024-02-15 15:02:34 +0800

    Update CardManager.cs

--- Assets/Prefab/Card/CardManager.cs
+++ Assets/Prefab/Card/CardManager.cs
@@ -5,18 +5,33 @@ using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
-    public List<GameObject> Card = new();
+    //public List<GameObject> Card = new();
+    public List<CardRemain.Cards> cards;
     public List<int> CardtoDrugList;
     //[SerializeField] PlayerAttack _pa;
     public Button drawbutton;
 
+    /*private void Start()
+    {
+        cards = GetComponent<CardRemain>().CardsAmount;
+    }*/
+    private void Update()
+    {
+        
+    }
+    
     public void Onclick()
     {
         for (int i = 0; i < 5; i++)
         {
-            int RandomIndex = UnityEngine.Random.Range(0, Card.Count - 1);
-            GameObject card = Instantiate(Card[RandomIndex], new Vector2(2 + i * 75, 10), Quaternion.identity);
+            int RandomIndex = GetRandomIndex();
+            if ()
+            {
+                break;
+            }
+            GameObject card = Instantiate(cards[RandomIndex].Cardobj, new Vector2(2 + i * 75, 10), Quaternion.identity);
             card.transform.SetParent(GameObject.FindGameObjectWithTag("UUI").transform, false);
+            
         }
         drawbutton.enabled = false;//抽卡按鈕關閉
         UseDraw();
@@ -28,7 +43,13 @@ public class CardManager : MonoBehaviour
         drawbutton.enabled = true;
     }
 
-    void UseDraw()//觸發計時器用
+    private int GetRandomIndex()
+    {
+        return Random.Range(0, cards.Count - 1);
+    }
+
+
+    private void UseDraw()//觸發計時器用
     {
         StartCoroutine(DrawCoolDown());
     }
