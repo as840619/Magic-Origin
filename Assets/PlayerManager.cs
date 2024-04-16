072766689cc30601dad8a5736535b25f1a28c8f9
HunterM <as22281624@gmail.com>
2024-04-16 15:30:19 +0800

    Update PlayerManager.cs
    -新增體力值，體力目前最高6點，體力每秒恢復1點，使用體力後恢復體力的速度下降75%，持續1秒

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
+    public TextMeshProUGUI staminaValueText;
+    public TextMeshProUGUI staminaMaxValueText;
     public int shieldValue;
+    public bool weakness;
+    public float weaknessTime;
+    public int staminaValue = 6;
+    public int staminaMaxValue = 6;
+    float tempTime;
 
     private static PlayerManager instance;
     public static PlayerManager Instance
@@ -14,6 +22,34 @@ public class PlayerManager : MonoBehaviour
             if (instance == null)
                 instance = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
             return instance;
+        }
+    }
+    private void Start()
+    {
+        staminaValueText.text = staminaValue.ToString();
+        staminaMaxValueText.text = staminaMaxValue.ToString();
+
+    }
+
+    private void Update()
+    {
+        staminaValueText.text = staminaValue.ToString();
+        staminaMaxValueText.text = staminaMaxValue.ToString();
+        if (staminaValue >= staminaMaxValue)
+            return;
+        if (weakness)
+        {
+            weaknessTime += Time.deltaTime;
+            tempTime += Time.deltaTime / 4;
+            if (weaknessTime >= 0.5f)
+                weakness = false;
+        }
+        else
+            tempTime += Time.deltaTime;
+        if (tempTime >= 1)
+        {
+            staminaValue += ((int)tempTime);
+            tempTime = 0;
         }
     }
 }
