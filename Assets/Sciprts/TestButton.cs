f7416a8837b28572a88a191bbec8b51251e0e119
HunterM <as22281624@gmail.com>
2024-04-18 09:28:47 +0800

    Update TestButton.cs
    -讓開發者模式更明顯一了

--- Assets/Sciprts/TestButton.cs
+++ Assets/Sciprts/TestButton.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class TestButton : MonoBehaviour
 {
+    bool invincible = false;
     public void Test()
     {
-        PlayerController.Instance.invincible = true;
+        if (invincible == false)
+        {
+            print("上帝模式開啟");
+            invincible = true;
+            PlayerController.Instance.invincible = true;
+            PlayerController.Instance.createrMode = true;
+        }
+        else
+        {
+            print("上帝模式關閉");
+            invincible = false;
+            PlayerController.Instance.invincible = false;
+            PlayerController.Instance.createrMode = false;
+        }
     }
 }
