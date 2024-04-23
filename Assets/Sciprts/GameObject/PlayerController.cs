859e81322f51687ef442c4b3c42597c3c344c52e
HunterM <as22281624@gmail.com>
2024-04-23 15:02:13 +0800

    Bug修正
    -排解隱藏房間沒有正常顯示

--- Assets/Sciprts/GameObject/PlayerController.cs
+++ Assets/Sciprts/GameObject/PlayerController.cs
@@ -283,7 +283,7 @@ public class PlayerController : MonoBehaviour
         {
         }
     }
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("HiddenTilemap"))
         {
