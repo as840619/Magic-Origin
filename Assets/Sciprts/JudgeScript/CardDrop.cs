d25ded434b89224a6ba6aec73a81c94a301fe6ae
HunterM <as22281624@gmail.com>
2024-04-30 16:17:10 +0800

    Update CardDrop.cs
    -使掉落的卡牌碰觸到水會消失

--- Assets/Sciprts/JudgeScript/CardDrop.cs
+++ Assets/Sciprts/JudgeScript/CardDrop.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CardDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Water"))
+            Destroy(gameObject);
     }
 }
