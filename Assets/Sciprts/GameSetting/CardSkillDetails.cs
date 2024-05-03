c61ccfd9e869c2bae7d54efb759093a53db15f90
HunterM <as22281624@gmail.com>
2024-05-03 18:03:19 +0800

    更改卡牌技能列表

--- Assets/Sciprts/GameSetting/CardSkillDetails.cs
+++ Assets/Sciprts/GameSetting/CardSkillDetails.cs
@@ -7,19 +7,10 @@ public class CardSkillDetails : MonoBehaviour
     [System.Serializable]
     public struct Skills
     {
+        public string name;
         public int damage;
         public bool addOrNot;
         public string addition;
     }
-
-    public List<Skills> Attack = new();
-    public List<Skills> Slash = new();
-    public List<Skills> Smash = new();
-    public List<Skills> SplitSlash = new();
-    public List<Skills> Spin = new();
-    public List<Skills> QuickStab = new();
-    public List<Skills> DashBlock = new();
-    public List<Skills> GloryShield = new();
-    public List<Skills> IronCastle = new();
-    public List<Skills> Block = new();
+    public List<Skills> SkillList = new();
 }
