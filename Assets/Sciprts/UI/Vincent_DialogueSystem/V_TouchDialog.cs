05432821a46813210cceecaabc3333041e2d4d8c
小飛 <vaz1306011@gmail.com>
2024-05-07 12:11:01 +0800

    對話框觸發修復

--- Assets/Sciprts/UI/Vincent_DialogueSystem/V_TouchDialog.cs
+++ Assets/Sciprts/UI/Vincent_DialogueSystem/V_TouchDialog.cs
@@ -1,15 +1,13 @@
-using Pathfinding.Ionic.Zip;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class V_TouchDialog : MonoBehaviour
 {
-    public GameObject DialScript;
+    public V_Dialog DialScript;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("Player"))
             return;
         DialScript.gameObject.SetActive(true);
+        DialScript.StartDialogue();
     }
 }
