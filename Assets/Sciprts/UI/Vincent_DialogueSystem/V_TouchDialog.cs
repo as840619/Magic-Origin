6012bf756b2990ebd16a7a31190129d412b50938
小飛 <50329983+vaz1306011@users.noreply.github.com>
2024-05-07 10:01:06 +0800

    對話觸發方式優化 (#44)

--- Assets/Sciprts/UI/Vincent_DialogueSystem/V_TouchDialog.cs
+++ Assets/Sciprts/UI/Vincent_DialogueSystem/V_TouchDialog.cs
@@ -6,18 +6,10 @@ using UnityEngine;
 public class V_TouchDialog : MonoBehaviour
 {
     public GameObject DialScript;
-    Rigidbody2D playerRb;
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        playerRb = GameObject.FindWithTag("Player")?.GetComponent<Rigidbody2D>();
-        if (GameObject.FindWithTag("Player"))
-        {
-            DialScript.gameObject.SetActive(true);
-        }
-        if (playerRb != null && DialScript != null)
-        {
-            DialScript.GetComponent<V_Dialog>().EndDialogue();
-        }
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+        DialScript.gameObject.SetActive(true);
     }
 }
