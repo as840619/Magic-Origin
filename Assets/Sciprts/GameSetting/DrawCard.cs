49a0a76391aa4f56ec67f324111c7320e64fcf7d
momobig0119 <vincent.mine@outlook.com>
2024-05-15 11:28:21 +0800

    FIX
    也許修復完計時器可能太快讀完的問題

--- Assets/Sciprts/GameSetting/DrawCard.cs
+++ Assets/Sciprts/GameSetting/DrawCard.cs
@@ -12,8 +12,10 @@ public class DrawCard : MonoBehaviour
     public GameObject changeColorPanel;
     Color32 inv = new Color32(255,255,255,0);
     Color32 grayy = new Color32(43, 43, 43, 221); 
+    private bool isCoolDown = false;
     public void Onclick()
     {
+        if(isCoolDown)return;
         CardManager.Instance.HandCards2GraveYard();
         for (int i = 0; i < MAXHANDCARD; i++)
         {
@@ -39,9 +41,11 @@ public class DrawCard : MonoBehaviour
 
     private IEnumerator DrawCoolDown()   //計時器
     {
+        isCoolDown = true;
         yield return new WaitForSecondsRealtime(5/*5*/);//真實秒數
         drawbutton.enabled = true;
         changeColorPanel.GetComponent <Image>().color = inv;
+        isCoolDown = false;
     }
 
     private void UseDraw()//觸發計時器用
